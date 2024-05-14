d7f3b2407f8c325c36441826303e107bdd0cdc76
Lại Quang Khải <104859097+khailq0909@users.noreply.github.com>
2024-05-14 20:13:31 +0700

    auth with google

--- client/src/pages/public/Login.jsx
+++ client/src/pages/public/Login.jsx
@@ -3,12 +3,16 @@ import * as Toast from '../../common/Toast'
 import { login } from '../../store/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { apiLogin } from '../../apis';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+import path from '../../ultils/path';
 import ForgotPassword from '../../components/ForgotPassword';
 
 function Login() {
   const navigate = useNavigate()
   const dispatch = useDispatch();
+  const [error, setError] = useState({
+    err:''
+  });
   const [openFg, setOpenFg] = useState(false)
   const [credentials, setCredentials] = useState({
     email: '',
@@ -17,14 +21,22 @@ function Login() {
 
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }))
+    setError({err: ""})
   };
   const handleLogin = async () => {
-    const rs = await apiLogin(credentials)
-    if (rs) {
-      dispatch(login({ isLoggedIn: true, token: rs.accessToken, current: rs.userData }))
+    try {
+      const rs = await apiLogin(credentials)
+      dispatch(login({ isLoggedIn: true, token: rs?.accessToken, current: rs?.userData }))
       Toast.toastSuccess('Login Success')
       navigate('/')
+    }catch (err) {
+      console.log(err)
+      setError({err: err?.response?.data.mes})  
     }
+
+  }
+  const hanldeGoogleLogin = () =>{
+    window.open(`${process.env.REACT_APP_API_URL}/auth/google/callback`, "_self")
   }
 
   return (
@@ -40,15 +52,20 @@ function Login() {
           <div className="group-input flex flex-col mt-5">
             <input type="password" id='passWord' placeholder='Password' className='border p-3 outline-black' onChange={handleChange} />
           </div>
-          <span className='mt-6 text-blue-500 cursor-pointer' onClick={() => setOpenFg(true)} >forgot password?</span>
-          <div className="btn-login w-full mt-6 text-center bg-main pb-5 pt-5 cursor-pointer text-white font-medium" onClick={handleLogin}>
+          {error && <span className='text-main mt-3'>{error.err}</span>}
+          <span className='mt-2 text-blue-500 cursor-pointer' onClick={() => setOpenFg(true)} >forgot password?</span>
+          <div className="btn-login w-full mt-5 mb-5 text-center bg-main pb-5 pt-5 cursor-pointer text-white font-medium" onClick={handleLogin}>
             Login
+          </div>
+          <Link to={`/${path.REGISTER}`}><span>Dont have account? <span className='text-blue-500 cursor-pointer'>register here</span></span></Link>
+          <div className="googleAuth p-2 bg-main cursor-pointer" onClick={hanldeGoogleLogin}>
+            Login with google
           </div>
         </div>
       </div>
       {
         openFg &&
-        <ForgotPassword setOpenFg={(data)=>setOpenFg(data)} />
+        <ForgotPassword setOpenFg={(data) => setOpenFg(data)} />
       }
 
       {
